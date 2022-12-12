d69db6a3e94eaf5ae1d2ba9667c8159d411956c5
BRP <bernard.robsons@gmail.com>
2022-12-12 15:13:53 -0800

    Updated final task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,20 @@
-#!/bin/bash
 #include <stdio.h>
+
 #include <unistd.h>
+
 /**
- * main - Entry point
- * Return:1
+ *main - Entry point
+ *Return:1
  */
+
 int main(void)
+
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+
+	fputs("and that piece of art is useful");
+	fputs("- Dora kORPAR, 2015-10-19");
+	fputs("stdout");
+
 	return (1);
+
 }
