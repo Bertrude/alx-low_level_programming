10f92b57f6c25d2e679bba7daef382f61ffd9e7b
BRP <bernard.robsons@gmail.com>
2022-12-12 15:18:13 -0800

    update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,10 +10,7 @@
 int main(void)
 
 {
-
-	fputs("and that piece of art is useful");
-	fputs("- Dora kORPAR, 2015-10-19");
-	fputs("stdout");
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 
 	return (1);
 
