8049007e78b2639be6895d58be0da24769da94d2
BRP <bernard.robsons@gmail.com>
2022-12-18 17:19:30 -0800

    Updated Task 101

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -14,20 +14,22 @@ int main(void)
 
 	for (h = 0 ; h != 8 ; h++)
 	{
-		i = n + 1;
-		do {
-			putchar('0' + h);
-			putchar('0' + n);
-			putchar('0' + i);
-			if (h != 7)
-			{
-				putchar(',');
-				putchar(32)
-			}
-			i++;
-		} while (i != 10);
+		for (n = h + 1 ; n != 9 ; n++)
+		{
+			i = n + 1;
+			do {
+				putchar('0' + h);
+				putchar('0' + n);
+				putchar('0' + i);
+				if (h != 7)
+				{
+					putchar(',');
+					putchar(32);
+				}
+				i++;
+			} while (i != 10);
+		}
 	}
-}
-putchar('\n');
-return (0);
+	putchar('\n');
+	return (0);
 }
