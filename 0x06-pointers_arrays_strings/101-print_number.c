aa43de697d96315d2161f4ba78367d44363bf985
BRP <bernard.robsons@gmail.com>
2022-12-22 17:22:15 -0800

    101-print_numbers

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_number -print number putchar
+ * @n:integer
+ * Return: void
+ */
+void print_number(int n)
+{
+	unsigned int x = n;
+
+	if (n < 0)
+	{
+		putchar('-');
+		x = -x;
+	}
+	if ((x / 10) > 0)
+		print_number(x / 10);
+
+	putchar(x % 10 + '0');
+}
